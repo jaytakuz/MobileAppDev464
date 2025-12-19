57506a6ae9254e3513fc239e9158398b6592283b
Watcharapong Wanna <jaytakuz@users.noreply.github.com>
2025-12-19 10:00:32 +0700

    Using dynamic list tile

--- lib/pages/contact.dart
+++ lib/pages/contact.dart
@@ -8,15 +8,26 @@ class ContactPage extends StatefulWidget {
 }
 
 class _ContactPageState extends State<ContactPage> {
-  List mydata = ['apple', 'banana', 'papaya'];
+  final items = List<String>.generate(1000, (i) => "Item$i");
+  // List mydata = ['apple', 'banana', 'papaya'];
   @override
   Widget build(BuildContext context) {
-    return ListView(
-      children: [
-        ListTile(onTap: () {}, leading: FlutterLogo(), title: Text(mydata[0]), subtitle: Text('subtitle'), trailing: FlutterLogo()),
-        ListTile(onTap: () {}, leading: FlutterLogo(), title: Text(mydata[1]), subtitle: Text('subtitle'), trailing: FlutterLogo()),
-        ListTile(onTap: () {}, leading: FlutterLogo(), title: Text(mydata[2]), subtitle: Text('subtitle'), trailing: FlutterLogo()),
-      ],
+    return ListView.builder(
+      itemBuilder: (context, index) {
+        return ListTile(
+          onTap: () {},
+          leading: Icon(Icons.access_alarm_outlined),
+          title: Text(items[index]),
+        );
+      },
     );
+
+    // return ListView(
+    //   children: [
+    //     ListTile(onTap: () {}, leading: FlutterLogo(), title: Text(mydata[0]), subtitle: Text('subtitle'), trailing: FlutterLogo()),
+    //     ListTile(onTap: () {}, leading: FlutterLogo(), title: Text(mydata[1]), subtitle: Text('subtitle'), trailing: FlutterLogo()),
+    //     ListTile(onTap: () {}, leading: FlutterLogo(), title: Text(mydata[2]), subtitle: Text('subtitle'), trailing: FlutterLogo()),
+    //   ],
+    // );
   }
 }

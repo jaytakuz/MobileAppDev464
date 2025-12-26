657481e7b55cf302caa16623e19565f46042ebe9
Watcharapong Wanna <jaytakuz@users.noreply.github.com>
2025-12-26 10:33:00 +0700

    Apply JSON Local Data

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:firstapp/pages/detail.dart';
 import 'package:flutter/material.dart';
 
@@ -11,43 +13,35 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Padding(
+    return Scaffold(
+    body: Padding(
       padding: const EdgeInsets.all(8.0),
-      child: ListView(
-        children: [
-          MyBox(
-            "What is a computer",
-            "Computer is an electronic device that processes data and performs a wide range of tasks by following a set of instructions, typically referred to as programs or software. It can execute complex calculations, store and retrieve data, and perform tasks like word processing, gaming, web browsing, data analysis, and much more.",
-            "https://media.licdn.com/dms/image/v2/D5612AQGPAWjwV6d5VQ/article-cover_image-shrink_720_1280/article-cover_image-shrink_720_1280/0/1704213035509?e=2147483647&v=beta&t=hfyEB-HTJSaOVzv4wlvmnqAgqEko_9_Rf4onikFSHHo",
-          ),
-          SizedBox(height: 24),
-          MyBox(
-            "What is a machine learning",
-            "Machine learning is a type of Artificial Intelligence that trains algorithms on large datasets to recognize patterns, learn from experience, and make predictions or decisions without being explicitly programmed for every task.",
-            "https://media.licdn.com/dms/image/v2/D5612AQGPAWjwV6d5VQ/article-cover_image-shrink_720_1280/article-cover_image-shrink_720_1280/0/1704213035509?e=2147483647&v=beta&t=hfyEB-HTJSaOVzv4wlvmnqAgqEko_9_Rf4onikFSHHo",
-          ),
-          SizedBox(height: 24),
-          MyBox(
-            "What is an artificial intelligence",
-            "Artificial intelligence is the simulation of human intelligence in machines that are programmed to think and learn like humans. AI systems can perform tasks that typically require human intelligence, such as visual perception, speech recognition, decision-making, and language translation.",
-            "https://media.licdn.com/dms/image/v2/D5612AQGPAWjwV6d5VQ/article-cover_image-shrink_720_1280/article-cover_image-shrink_720_1280/0/1704213035509?e=2147483647&v=beta&t=hfyEB-HTJSaOVzv4wlvmnqAgqEko_9_Rf4onikFSHHo",
-          ),
-          SizedBox(height: 24),
-        ],
+      child: FutureBuilder(
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            var data = json.decode(snapshot.data.toString());
+            return ListView.builder(itemBuilder: (BuildContext context, int index) {
+              return MyBox(data[index]['title'], data[index]['subtitle'], data[index]['image_url']);
+            },
+            itemCount: data.length,);
+          }
+          return const Center(child: CircularProgressIndicator());
+        },
+        future: DefaultAssetBundle.of(context).loadString('data.json'),)
       ),
     );
-    // return ListView(children: [Center(child: Text("HomePage"))]);
   }
 
-  Widget MyBox(String title, String subtitle, String img_url) {
+  Widget MyBox(String title, String subtitle, String image_url) {
     return Container(
+      margin: EdgeInsets.only(top: 20),
       padding: EdgeInsets.all(24.0),
       height: 252,
       decoration: BoxDecoration(
         color: Colors.deepPurpleAccent,
         borderRadius: BorderRadius.circular(24),
         image: DecorationImage(
-          image: NetworkImage(img_url),
+          image: NetworkImage(image_url),
           fit: BoxFit.cover,
           colorFilter: ColorFilter.mode(
             Colors.black.withOpacity(0.5),
@@ -72,14 +66,16 @@ class _HomePageState extends State<HomePage> {
           TextButton(
             onPressed: () {
               print("Next Page");
-              Navigator.push(context, MaterialPageRoute(builder: (context) => DetailsPage()));
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => DetailsPage()),
+              );
             },
-            child: 
-            Text(
+            child: Text(
               "read more",
-              style: TextStyle(color: Colors.lightGreen)
-            )
-          )
+              style: TextStyle(color: Colors.lightGreen),
+            ),
+          ),
         ],
       ),
     );
